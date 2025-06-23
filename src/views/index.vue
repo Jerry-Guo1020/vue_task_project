a7e72045cb8522ed8065d7834f2e4cd25d7ca53a
Jerry <2593517388@qq.com>
2025-06-24 00:24:22 +0800

    学习了部分内容

--- src/views/index.vue
+++ src/views/index.vue
@@ -6,11 +6,17 @@
     <!-- 根据任务数量判断 -->
     <div v-if="task.taskList.length > 0">
         <h2>任务列表,当前的任务有：</h2>
-        <div class="card" v-for="(item, index) in task.taskList" :key="index">
+        <div class="card" v-for="(item, index) in task.taskList" :key="index"
+            :style="{ color: item.completed ? 'red' : 'black' }">
             任务{{ index + 1 }}:
             <br>
             {{ item.content }}
 
+            <button @click="task.completeTask(index)">
+                {{ item.completed ? "未完成" : "完成" }}
+            </button>
+
+            <button @click="task.deleteTask(index)">删除</button>
         </div>
     </div>
     <div v-else> 当前没有任务哦</div>
@@ -39,4 +45,16 @@ const handle = () => {
 
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.card {
+    padding: 10px;
+    margin-top: 10px;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    transition: all 0.2s ease;
+}
+
+.card button {
+    margin-left: 10px;
+}
+</style>
